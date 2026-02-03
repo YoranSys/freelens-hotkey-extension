5c38bb95459bfce98120ebde13739492ab1c3263
Yohann Fabri <11197007+YoranSys@users.noreply.github.com>
2026-02-03 15:17:04 -0400

    Remove unused imports and clean up ExampleRenderer class

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,2 @@
 import { Main } from "@freelensapp/extensions";
-import { ExamplePreferencesStore } from "../common/store";
-
-export default class ExampleMain extends Main.LensExtension {
-  async onActivate() {
-    await ExamplePreferencesStore.getInstanceOrCreate().loadExtension(this);
-  }
-}
+export default class ExampleMain extends Main.LensExtension {}
